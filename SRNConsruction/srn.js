94129c24b2f98737c41f6e65d0f252bfe0c8af7e
abodkhe1 <122731356+abodkhe1@users.noreply.github.com>
2023-01-15 20:03:16 +0530

    my website

---
+++ SRNConsruction/srn.js
@@ -0,0 +1,32 @@
+// function form(){
+//     var a=document.getElementById("f_name").value
+//     var b=document.getElementById("l_name").value
+//     var c=document.getElementById("sub").value
+//     var d=document.getElementById("texrarea").value
+//     document.getElementById("answer").innerHTML=(a+" "+b)+"Submited Successfuly"
+
+// }
+// function a(){
+//     var x=document.getElementById("date")
+    
+//     // x.innerHTML=new Date().toLocaleDateString()+"<br>"
+//     x.innerHTML=new Time().to
+//     x.innerHTML=new Date().
+
+    
+// }
+// setTimeout(a,2000)
+// // setInterval(a,3000)
+function service(){
+alert("Welcome to our services")
+}
+function continuef(){
+alert("Continue")
+}
+// function validateForm() {
+//     let x = document.forms["myForm"]["firstname"].value;
+//     if (x == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     }
+//   }
